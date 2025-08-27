f6801c7ad1506a0e1997d0097b2db6abd5e9045d
Somy Garg <lovegarg236@gmail.com>
2025-08-27 17:07:44 +0530

    Hibernate and jpa

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO test.product_table (price, quantity, sku, title_x )
+VALUES (12.4,4, 'parle734', 'parlebiscuit'),
+       (14.4,1, 'pepsi123', 'pepsi'),
+       (34.4,10, 'coke124', 'Coca Cola'),
+		(16.4,3, 'pepsi156', 'Mazza');
---
+++ src/test/java/com/codingshuffle/jpaTutorial/jpaTuts/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuffle.jpaTutorial.jpaTuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
